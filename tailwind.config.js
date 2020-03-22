69c46c853b8edbbaa91942fb2df22503ed734368
Hasibul Huda <hasibulhuda1@gmail.com>
2020-03-22 20:11:49 +0600

    :tada: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      height:{
+        14: "56px"
+      },
+      width:{
+        14: "56px"
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
